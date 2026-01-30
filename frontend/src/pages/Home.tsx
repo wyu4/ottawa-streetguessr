ae3ed0a7013e66c4521a7c16399be3b02d5fdc31
wyu4 <wilsonyu657@gmail.com>
2026-01-30 18:06:08 -0500

    Beautify home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import "../styles/Home.scss";
 import Widget from "../components/Widget";
+import Parliament from "/Parliament.webp";
 
 export default function Home() {
     return (
-        <div className="home background">
+        <div className="home">
+            <img className="background" src={Parliament} draggable={false} />
             <h1>Ottawa StreetGuessr</h1>
             <Widget className="play">
-                <button> <p>{">"}</p></button>
+                <button>
+                </button>
             </Widget>
         </div>
     );
